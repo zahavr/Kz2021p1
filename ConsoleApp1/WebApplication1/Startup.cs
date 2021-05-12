3f1c5ca75c7e1b628d74dad31bb65f26bb88b3d5
zahavr <leshabiceps34@gmail.com>
2021-05-12 14:46:39 +0600

    Рефракторинг регистрации репозиториев. Можно регистрировать с наличием интерфейса или без интерфейса.

--- ConsoleApp1/WebApplication1/Startup.cs
+++ ConsoleApp1/WebApplication1/Startup.cs
@@ -30,136 +30,148 @@ using WebApplication1.EfStuff.Repositoryies.Interface;
 namespace WebApplication1
 {
 	public class Startup
-    {
-        public const string AuthMethod = "Smile";
+	{
+		public const string AuthMethod = "Smile";
 
-        public Startup(Microsoft.Extensions.Configuration.IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+		public Startup(Microsoft.Extensions.Configuration.IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public Microsoft.Extensions.Configuration.IConfiguration Configuration { get; }
+		public Microsoft.Extensions.Configuration.IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews().AddNewtonsoftJson();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews().AddNewtonsoftJson();
 			services.AddOpenApiDocument();
 			services.AddRazorPages()
 				 .AddRazorRuntimeCompilation();
 
 			var connectionString = Configuration.GetValue<string>("SpecialConnectionStrings");
-            services.AddDbContext<KzDbContext>(option => option.UseSqlServer(connectionString));
+			services.AddDbContext<KzDbContext>(option => option.UseSqlServer(connectionString));
 
-            RegisterRepositories(services);
+			RegisterRepositories(services);
 
-            services.AddScoped<IUserService>(x =>
-                new UserService(
-                    x.GetService<ICitizenRepository>(),
-                    x.GetService<IHttpContextAccessor>())
-                );
+			services.AddScoped<IUserService>(x =>
+				new UserService(
+					x.GetService<ICitizenRepository>(),
+					x.GetService<IHttpContextAccessor>())
+				);
 
-            services.AddScoped<CitizenPresentation>(x =>
-                new CitizenPresentation(
-                    x.GetService<ICitizenRepository>(),
-                    x.GetService<IUserService>(),
-                    x.GetService<IMapper>()));
+			services.AddScoped<CitizenPresentation>(x =>
+				new CitizenPresentation(
+					x.GetService<ICitizenRepository>(),
+					x.GetService<IUserService>(),
+					x.GetService<IMapper>()));
 
-            services.AddPoliceServices(Configuration);
-            RegisterAutoMapper(services);
+			services.AddPoliceServices(Configuration);
+			RegisterAutoMapper(services);
 
-            services.AddAuthentication(AuthMethod)
-                .AddCookie(AuthMethod, config =>
-                {
-                    config.Cookie.Name = "Smile";
-                    config.LoginPath = "/Citizen/Login";
-                    config.AccessDeniedPath = "/Citizen/Login";
-                });
+			services.AddAuthentication(AuthMethod)
+				.AddCookie(AuthMethod, config =>
+				{
+					config.Cookie.Name = "Smile";
+					config.LoginPath = "/Citizen/Login";
+					config.AccessDeniedPath = "/Citizen/Login";
+				});
 
-            services.AddHttpContextAccessor();
-            services.AddPaging();
-        }
+			services.AddHttpContextAccessor();
+			services.AddPaging();
+		}
 
-        private void RegisterRepositories(IServiceCollection services)
-        {
-            foreach (var repositoryType in Assembly
-                .GetExecutingAssembly()
-                .GetTypes()
-                .Where(type =>
-                        type.BaseType?.IsGenericType == true
-                        && type.BaseType.GetGenericTypeDefinition() == typeof(BaseRepository<>)))
-            {
-                services.AddScoped(repositoryType, x =>
-                {
-                    var constructor = repositoryType.GetConstructors().Single();
-                    var parameters = new object[] { x.GetService<KzDbContext>() };
-                    return constructor.Invoke(parameters);
-                });
-            }
-        }
+		private void RegisterRepositories(IServiceCollection services)
+		{
+			var repositoryTypes = Assembly
+				.GetExecutingAssembly()
+				.GetTypes()
+				.Where(type =>
+						type.BaseType?.IsGenericType == true
+						&& type.BaseType.GetGenericTypeDefinition() == typeof(BaseRepository<>));
 
-        private void RegisterAutoMapper(IServiceCollection services)
-        {
-            var configurationExp = new MapperConfigurationExpression();
+			foreach (var repositoryType in repositoryTypes)
+			{
+				var repositortInterfaces = repositoryType.GetInterfaces()
+					.FirstOrDefault(i => i.Name != typeof(IBaseRepository<>).Name);
 
-            configurationExp.CreateMap<Adress, AdressViewModel>()
-                .ForMember(nameof(AdressViewModel.CitizenCount),
-                    opt => opt.MapFrom(adress => adress.Citizens.Count()));
-            configurationExp.CreateMap<AdressViewModel, Adress>();
+				if (repositortInterfaces != null)
+				{
+					services.AddScoped(repositortInterfaces, repositoryType);
+				}
+				else
+				{
+					services.AddScoped(repositoryType, x =>
+					{
+						var constructor = repositoryType.GetConstructors().Single();
+						var parameters = new object[] { x.GetService<KzDbContext>() };
+						return constructor.Invoke(parameters);
+					});
+				}
+			}
+		}
 
-            configurationExp.CreateMap<IncomingFlightInfo, IncomingFlightInfoViewModel>();
-            configurationExp.CreateMap<IncomingFlightInfoViewModel, IncomingFlightInfo>();
+		private void RegisterAutoMapper(IServiceCollection services)
+		{
+			var configurationExp = new MapperConfigurationExpression();
 
-            configurationExp.CreateMap<DepartingFlightInfo, DepartingFlightInfoViewModel>();
-            configurationExp.CreateMap<DepartingFlightInfoViewModel, DepartingFlightInfo>();
-            
-            configurationExp.AddProfile<PoliceProfiles>();
+			configurationExp.CreateMap<Adress, AdressViewModel>()
+				.ForMember(nameof(AdressViewModel.CitizenCount),
+					opt => opt.MapFrom(adress => adress.Citizens.Count()));
+			configurationExp.CreateMap<AdressViewModel, Adress>();
 
-            configurationExp.CreateMap<Fireman, FiremanShowViewModel>()
-                .ForMember(nameof(FiremanShowViewModel.Name),
-                        opt => opt.MapFrom(fireman => fireman.Citizen.Name))
-                .ForMember(nameof(FiremanShowViewModel.Age),
-                        opt => opt.MapFrom(fireman => fireman.Citizen.Age));
+			configurationExp.CreateMap<IncomingFlightInfo, IncomingFlightInfoViewModel>();
+			configurationExp.CreateMap<IncomingFlightInfoViewModel, IncomingFlightInfo>();
 
-            configurationExp.CreateMap<FiremanShowViewModel, Fireman>();
+			configurationExp.CreateMap<DepartingFlightInfo, DepartingFlightInfoViewModel>();
+			configurationExp.CreateMap<DepartingFlightInfoViewModel, DepartingFlightInfo>();
 
-            MapBothSide<Fireman, FiremanViewModel>(configurationExp);
-            MapBothSide<Citizen, FullProfileViewModel>(configurationExp);
-            MapBothSide<Bus, BusParkViewModel>(configurationExp);
-            MapBothSide<TripRoute, TripViewModel>(configurationExp);
+			configurationExp.AddProfile<PoliceProfiles>();
 
-            var config = new MapperConfiguration(configurationExp);
-            var mapper = new Mapper(config);
-            services.AddScoped<IMapper>(x => mapper);
-        }
+			configurationExp.CreateMap<Fireman, FiremanShowViewModel>()
+				.ForMember(nameof(FiremanShowViewModel.Name),
+						opt => opt.MapFrom(fireman => fireman.Citizen.Name))
+				.ForMember(nameof(FiremanShowViewModel.Age),
+						opt => opt.MapFrom(fireman => fireman.Citizen.Age));
 
-        public void MapBothSide<Type1, Type2>(MapperConfigurationExpression configurationExp)
-        {
-            configurationExp.CreateMap<Type1, Type2>();
-            configurationExp.CreateMap<Type2, Type1>();
-        }
+			configurationExp.CreateMap<FiremanShowViewModel, Fireman>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			MapBothSide<Fireman, FiremanViewModel>(configurationExp);
+			MapBothSide<Citizen, FullProfileViewModel>(configurationExp);
+			MapBothSide<Bus, BusParkViewModel>(configurationExp);
+			MapBothSide<TripRoute, TripViewModel>(configurationExp);
 
-            app.UseRouting();
+			var config = new MapperConfiguration(configurationExp);
+			var mapper = new Mapper(config);
+			services.AddScoped<IMapper>(x => mapper);
+		}
 
-            app.UseAuthentication();
+		public void MapBothSide<Type1, Type2>(MapperConfigurationExpression configurationExp)
+		{
+			configurationExp.CreateMap<Type1, Type2>();
+			configurationExp.CreateMap<Type2, Type1>();
+		}
 
-            app.UseAuthorization();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
